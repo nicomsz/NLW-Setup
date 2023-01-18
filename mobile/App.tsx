032a9c10a9a19bc48ccdee9771b46e43fd93c3f7
Nicolas Moraes de Souza <76184413+NicolasMoraesDeSouza@users.noreply.github.com>
2023-01-18 16:46:14 -0300

    Adicionando componente de loading, adicionando statusbar e colocando icones

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
+import Loading from './src/assets/components/loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts( {
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+  if (!fontsLoaded) {
+    return <Loading />
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent/>
     </View>
   );
 }
@@ -13,7 +24,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09090A',
     alignItems: 'center',
     justifyContent: 'center',
   },
